b43fdb58e20b34f564f5cf0f4d8c1142b0c49107
Travis Longwell <travislongwell@yahoo.com>
2021-02-27 05:54:59 -0500

    Initial commit

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 "use strict";
 
+// improve console output 
+require('console-stamp')(console, 'HH:MM:ss.l');
+//require('console-stamp')(console, 'ddd mmm dd yyyy HH:MM:ss')
+
 var commandLineArgs  = require('command-line-args'),
     fs               = require('fs'),
     WebHooks         = require('node-webhooks'),
-    imgur            = require('imgur-node-api'),
-    RoonApi          = require("node-roon-api"),
+    RoonApi          = require('node-roon-api'),
     RoonApiSettings  = require('node-roon-api-settings'),
     RoonApiStatus    = require('node-roon-api-status'),
     RoonApiTransport = require('node-roon-api-transport'),
@@ -18,19 +21,17 @@ var image = undefined;
 var message = undefined;
 
 const optionDefinitions = [
-    { name: 'webhook', type: String },
-    { name: 'imgurid', type: String },
-    { name: 'zone', type: String }
+    { name: 'webhook', type: String }
 ];
 
 var args = commandLineArgs(optionDefinitions);
 
 var roon = new RoonApi({
-    extension_id:        'com.rhodium.roonbot',
-    display_name:        "RoonBot for Slack",
+    extension_id:        'com.travsplk.roonhec',
+    display_name:        "Roon to Splunk HEC",
     display_version:     "1.0.0",
-    publisher:           'Thomas Rosdahl',
-    email:               'thomas.rosdahl@telia.com',
+    publisher:           'Travis Longwell',
+    email:               'notreal@email.com',
 	core_paired: function(core_) {
         core = core_;
         image = core.services.RoonApiImage;
@@ -41,6 +42,10 @@ var roon = new RoonApi({
             } else if (response == "Changed") {
                 if (msg.zones_changed) {
                     zones = msg.zones_changed;
+                    zones_changed();
+                }
+                if (msg.zones_seek_changed) {
+                    //console.log(msg.zones_seek_changed);
                 }
                 if (msg.zones_added) {
                     zones = msg.zones_added;
@@ -61,71 +66,47 @@ var webHooks = new WebHooks({
 var emitter = webHooks.getEmitter()
   
 emitter.on('*.success', function (shortname, statusCode, body) {
-    console.log('Success on trigger webHook ' + shortname + ' with status code', statusCode, 'and body', body)
+    console.log('Info: Success on trigger webHook ' + shortname + ' with status code', statusCode, 'and body', body)
 })
  
 emitter.on('*.failure', function (shortname, statusCode, body) {
-    console.error('Error on trigger webHook ' + shortname + ' with status code', statusCode, 'and body', body)
+    console.error('Error: Error on trigger webHook ' + shortname + ' with status code', statusCode, 'and body', body)
 })
 
-webHooks.add('slack', args.webhook);
+//remove webHookdsDB.json if your first webhook attempts are unsuccessful
+webHooks.add('splunk', args.webhook); 
 
 roon.init_services({
     required_services: [ RoonApiTransport, RoonApiImage ]
 });
 
-imgur.setClientID(args.imgurid);
-
 roon.start_discovery();
 
-monitor();
-
-function monitor() {
+function zones_changed() {
     for (var i = 0; i < zones.length; i++) {
-        if (zones[i].display_name.toUpperCase() == args.zone.toUpperCase() && 
-            zones[i].now_playing != undefined) {
-            var now_playing = zones[i].now_playing;
-            if (trigger != now_playing.three_line.line3) {
-                trigger = now_playing.three_line.line3;
-                console.log('New trigger: ' + trigger);
-                var options = { scale: 'fit', width: 200, height: 200 };
-                console.log('Downloading image key=' + now_playing.image_key);
-                image.get_image(now_playing.image_key, options, function (error, content_type, image)
-                {
-                    if (error == true) { console.log('Error:' + error);  return; }
-                    fs.writeFile('image.tmp', image, function (err) {
-                        console.log('Uploading image');
-                        imgur.upload('image.tmp', function (err, res) {
-                            if (err != undefined) { console.log(err); return; }    
-                            if (res != undefined) {
-                                console.log('Triggering webhook')
-                                webHooks.trigger('slack', {attachments: [
-                                    {
-                                        "fallback": trigger,
-                                        "color": "#CCCCCC",
-                                        "title": trigger,
-                                        "title_link": 'https://www.google.com/search?q=' + encodeURIComponent(now_playing.three_line.line2 + ' ' + now_playing.three_line.line3),
-                                        "image_url": res.data.link,
-                                        "fields": [
-                                            {
-                                                "title": "Track",
-                                                "value": now_playing.three_line.line1,
-                                                "short": true
-                                            },
-                                            {
-                                                "title": "Artist",
-                                                "value": now_playing.three_line.line2,
-                                                "short": true
-                                            }
-                                        ]
-                                    }
-                                ]})
-                            }
-                        });
-                    });
-                });
-            }
+        var zone = zones[i];
+        var now_playing = zone.now_playing;
+        var playback_state = zone.state;
+        
+        console.log('Info: Building zone [' + zone.display_name + '] playback event [' + playback_state + ']');
+
+        //build event object
+        var splunk_event = {
+            "state": playback_state,
+            "zone_name": zone.display_name,
+            "zone_id": zone.zone_id,
+            "zone_full" : zone
         }
+
+        if (now_playing != undefined) {
+            splunk_event.title = now_playing.three_line.line1;
+            splunk_event.artist = now_playing.three_line.line2;
+            splunk_event.album = now_playing.three_line.line3;
+            splunk_event.length = now_playing.length;
+            splunk_event.seek_position = now_playing.seek_position;
+        }
+                
+        console.log('Info: Triggering webhook to send event')
+        webHooks.trigger('splunk', splunk_event);
     }
-    setTimeout(monitor, 10000);
 }
